de67c49e753dd2269fa13c7f63ebf66b2d096d9e
MrZebarth <kris.zebarth@ocsb.ca>
2017-04-18 13:00:50 -0400

    Added Project

---
+++ src/Run.java
@@ -0,0 +1,26 @@
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Make a roman numeral calculator. Read in two numbers in Roman, 
+		 * translate it into regular numbers, add them,
+		 * output in Roman Numerals.
+		 * M=1000
+		 * CM=900
+		 * D=500
+		 * CD=400
+		 * C=100
+		 * XC=90
+		 * L=50
+		 * XL=40
+		 * X=10
+		 * IX=9
+		 * V=5
+		 * IV=4
+		 * I=1
+		 */
+	}
+
+}
